1d91e3d7f1e5f2aca2d5812591915fba75499c31
Michał Łukasiewicz <michal@threesixty.com>
2020-03-28 23:41:48 +0100

    Add test project

---
+++ Simple DNN Tests/NeuralNetwork/NetworkTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Simple_DNN;
+
+namespace Simple_DNN_Tests
+{
+  [TestClass]
+  public class NetworkTests
+  {
+    [TestMethod]
+    public void Network__ForGivenNumberOfLayersAndElements_ShouldIterateOverAllElements()
+    {
+      int[] layers = { 10, 10, 10 };
+      int excpectedIterations = layers.ToList().Sum();
+      int iterations = 0;
+
+      Network network = new Network(layers);
+
+      network.ForEach(neuron => iterations++);
+
+      Assert.AreEqual(excpectedIterations, iterations);
+    }
+
+
+    //public void Network__ForGivenNumberOfLayersAndElements_ShouldIterateOverInputLayerElementsOnly()
+    //{
+
+    //}
+  }
+}
---
+++ Simple DNN Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Simple DNN Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Simple DNN Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4884c9fb-271a-469c-b9b0-cd61bf9a99ea")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
